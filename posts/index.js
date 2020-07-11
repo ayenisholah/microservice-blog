4d7805ccf987a9c9cccc7a29cb9a29e3ec5ae5e8
Shola Ayeni <ayenisholah@yahoo.com>
2020-07-12 00:06:26 +0100

    scaffolded post service

---
+++ posts/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(bodyParser.json());
+
+const post = {};
+
+app.get("/post", (req, res) => {
+  res.send(post);
+});
+
+app.post("/post", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  post[id] = { id, title };
+
+  res.status(201).send(post[id]);
+});
+
+app.listen(4000, () => {
+  console.log("Post service listening on port 4000");
+});
